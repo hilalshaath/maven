219dde372616c1044b5e7a9e7fa8680500dd6692
hilal <hilal.shaath@gmail.com>
2015-12-14 13:32:01 -0800

    first maven commit

---
+++ src/main/java/com/vogella/build/maven/intro/App.java
@@ -0,0 +1,13 @@
+package com.vogella.build.maven.intro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Util.printMessage( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/vogella/build/maven/intro/Util.java
@@ -0,0 +1,13 @@
+package com.vogella.build.maven.intro;
+
+/**
+ * A utility class
+ *
+ */
+public class Util 
+{
+    public static void printMessage( String message )
+    {
+     	System.out.println(message);
+    }
+}
